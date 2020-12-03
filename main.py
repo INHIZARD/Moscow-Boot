83f45648c614073836360bc5cd4cfe2343fefc9e
INHIZARD <danila-dan89@bk.ru>
2020-12-03 16:05:49 +0300

    uic-file

---
+++ main.py
@@ -0,0 +1,41 @@
+import sys
+
+from PyQt5 import uic
+from PyQt5.QtGui import QPainter, QColor
+from PyQt5.QtWidgets import QApplication, QMainWindow
+
+
+class MyWidget(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        uic.loadUi('UI.ui', self)
+        self.do_paint = False
+        self.pushButton.clicked.connect(self.paint)
+
+
+    def paintEvent(self, event):
+        if self.do_paint:
+            qp = QPainter()
+            qp.begin(self)
+            self.draw_circle(qp)
+            qp.end()
+            self.do_paint = False
+
+    def paint(self):
+        self.do_paint = True
+        self.repaint()
+
+    def draw_circle(self, qp):
+        qp.setBrush(QColor(255, 0, 0))
+        qp.drawRect(30, 30, 120, 30)
+        qp.setBrush(QColor(0, 255, 0))
+        qp.drawRect(30, 60, 120, 30)
+        qp.setBrush(QColor(0, 0, 255))
+        qp.drawRect(30, 90, 120, 30)
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    ex = MyWidget()
+    ex.show()
+    sys.exit(app.exec_())
